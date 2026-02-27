b18e6dad9f8cdbb422914ae266494a6e1c52b529
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-27 15:34:08 +0200

    Add solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,31 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Hello'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
   // write more tests here
+  it(`should return an array with a reversed string
+    if original array has only one sentence`, () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it(`should return an array with reverses
+    and keep original array string lengths
+    if original array has two or more sentences`, () => {
+    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`should return an array with reverses
+    and keep original array string lengths
+    if original array has two or more sentences
+    (including numbers and punctuation)`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
 });
